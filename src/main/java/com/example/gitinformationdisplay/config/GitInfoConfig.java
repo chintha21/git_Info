fe5caa48c7134e8749f95bb5c0e57c541d67b77f
chintha21 <40859956+chintha21@users.noreply.github.com>
2022-06-12 02:33:41 +0530

    Added classes

---
+++ src/main/java/com/example/gitinformationdisplay/config/GitInfoConfig.java
@@ -0,0 +1,20 @@
+package com.example.gitinformationdisplay.config;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.io.ClassPathResource;
+
+@Configuration
+public class GitInfoConfig {
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer placeholderConfigurer() {
+        PropertySourcesPlaceholderConfigurer propsConfig
+                = new PropertySourcesPlaceholderConfigurer();
+        propsConfig.setLocation(new ClassPathResource("git.properties"));
+        propsConfig.setIgnoreResourceNotFound(true);
+        propsConfig.setIgnoreUnresolvablePlaceholders(true);
+        return propsConfig;
+    }
+}
--- src/main/java/com/example/gitinformationdisplay/controller/GitInfoController.java
+++ src/main/java/com/example/gitinformationdisplay/controller/GitInfoController.java
@@ -1,10 +1,34 @@
 package com.example.gitinformationdisplay.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class GitInfoController {
+
+
+    @Value("${git.commit.message.short}")
+    private String commitMessage;
+
+    @Value("${git.branch}")
+    private String branch;
+
+    @Value("${git.commit.id}")
+    private String commitId;
+
+    @RequestMapping("/commitId")
+    public Map<String, String> getCommitId() {
+        Map<String, String> result = new HashMap<>();
+        result.put("Commit message",commitMessage);
+        result.put("Commit branch", branch);
+        result.put("Commit id", commitId);
+        return result;
+    }
 
     @GetMapping("/git")
     public String getGitInformation(){
